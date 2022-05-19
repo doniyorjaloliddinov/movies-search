17aec2d845c03d8ee47a49421f55d73625577a2d
doniyorjonjaloliddinov <don2003.01.09@gmail.com>
2022-05-19 12:52:51 +0500

    add some changes

--- js/script.js
+++ js/script.js
@@ -2,8 +2,7 @@
 let elSearchInput = selectorElem(".js-search");
 let elSearchBtn = selectorElem(".search-btn");
 let elReadyList = selectorElem(".movies-list");
-// let elTest = selectorElem(".js-list");
-
+let elTemplate = selectorElem("#template").content;
 
 // FUNCTION READY LIST
 let readyList = function () {
@@ -18,7 +17,7 @@ let readyList = function () {
 
         let elYear = createElem("p", `Year of premiere: ${kino.year}`, "movies-year t-center text-center");
 
-        let elActors = createElem("p", `Actors: ${kino.cast.join(",   ")}`, "movies-actors t-center text-center");
+        let elActors = createElem("p", `Actors: ${kino.cast.splice(0,5).join(",   ")}`, "movies-actors t-center text-center");
 
         elItems.append(elImg, elTitle, elYear, elActors);
         elReadyList.append(elItems);
@@ -27,41 +26,3 @@ let readyList = function () {
     });
 }
 readyList();
-
-// let renderElements = function() {
-//     let elMovieWrapperFragment = document.createDocumentFragment();
-
-//     kinolar.forEach(function(kino) {
-//         elMovieWrapperFragment.appendChild(readyList(kino));
-//     })
-//     elReadyList.appendChild(elMovieWrapperFragment);
-// }
-
-// renderElements(kinolar);    
-
-
-// CALLING INPUT VALUE
-elSearchBtn.addEventListener("click", function() {
-    let films = [];
-    let searchingValue = elSearchInput.value.trim();
-    elTest.value = readyList()
-    let elTestItem = createElem("li");
-
-    let searchRegex = new RegExp(searchingValue, "gi");
-    
-    kinolar.forEach((kino) => {
-        elTest.append(elTestItem);
-        if (kino.title.match(searchRegex)) {
-            // films.push(kino);
-            // films.push(elTest)
-            // elTest.innerHtml = elTest
-
-        }
-        // else {
-        //     console.log("No suckh kind of movie");
-        // }
-    })
-    // return kinolar
-    console.log(films);
-})
-// console.log(films);
